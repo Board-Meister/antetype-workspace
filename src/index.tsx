07e8db571f25135f3ad468eedcfb8d348901e936
michal.dzierzbicki <michal.dzierzbicki@thehutgroup.com>
2024-10-03 19:10:45 +0200

    Update to newer versions

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,18 @@
 import type { IInjectable } from "@boardmeister/marshal"
 import type { Minstrel } from "@boardmeister/minstrel"
 import type { Herald } from "@boardmeister/herald"
-import type React from "react"
-import type styled from "styled-components";
 
 interface IInjected extends Record<string, object> {
   minstrel: Minstrel;
   herald: Herald;
-  react: typeof React;
-  styled: typeof styled;
 }
 
 const Skeleton: IInjectable = class {
   injected?: IInjected;
 
   static inject: Record<string, string> = {
-    minstrel: 'boardmeister/minstrel:latest',
-    herald: 'boardmeister/herald:latest',
-    react: 'react/react:latest',
-    styled: 'react/styled:latest',
+    minstrel: 'boardmeister/minstrel',
+    herald: 'boardmeister/herald',
   }
   inject(injections: IInjected): void {
     this.injected = injections;
